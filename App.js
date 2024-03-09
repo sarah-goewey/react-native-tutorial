9aa63c541a0aa98330e6b1b0202d2e243dde8ece
Sarah Goewey <sarah.goewey@yahoo.com>
2024-03-09 12:36:36 -0500

    setup

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+  FlatList,
+} from "react-native";
+
+const DATA = [
+  {
+    id: "1",
+    title: "Meditation",
+  },
+  {
+    id: "2",
+    title: "Coding",
+  },
+  {
+    id: "3",
+    title: "Journaling",
+  },
+];
+
+const Todoitem = (props) => (
+  <View>
+    <Text>{props.item.title}</Text>
+  </View>
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sarah Goewey</Text>
       <StatusBar style="auto" />
+      <TextInput style={styles.input} />
+      <Button title="start" onPress={() => Alert.alert("Button pressed")} />
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Todoitem item={item} />}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
@@ -13,8 +49,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "gray",
   },
 });
