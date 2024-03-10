f560986a5c0ef7ce4cc5b3dc442d0e6c265560a5
Sarah Goewey <sarah.goewey@yahoo.com>
2024-03-10 13:44:07 -0400

    completed tutorial video

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { styles } from "./App.style";
 import {
   StyleSheet,
   Text,
@@ -7,57 +10,110 @@ import {
   TextInput,
   Alert,
   FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+  Modal,
 } from "react-native";
 
 const DATA = [
   {
     id: "1",
     title: "Meditation",
+    completed: false,
+    color: "#FE938C",
   },
   {
     id: "2",
     title: "Coding",
+    completed: false,
+    color: "#9D8DF1",
   },
   {
     id: "3",
     title: "Journaling",
+    completed: false,
+    color: "#235789",
   },
 ];
 
-const Todoitem = (props) => (
-  <View>
-    <Text>{props.item.title}</Text>
-  </View>
-);
-
 export default function App() {
+  const [items, setItems] = useState(DATA);
+  const [text, setText] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const addNewToDo = () => {
+    let newToDo = {
+      id: items.length + 1,
+      title: text,
+      completed: false,
+      color: "#9EBC9E",
+    };
+
+    setItems([...items, newToDo]);
+    setText("");
+    setIsModalVisible(false);
+  };
+
+  const markItemCompleted = (item) => {
+    const itemIndex = items.findIndex((i) => i.id === item.id);
+
+    if (itemIndex >= 0) {
+      const itemsToUpdate = [...items];
+      itemsToUpdate[itemIndex] = { ...items[itemIndex], completed: true };
+      setItems(itemsToUpdate);
+    }
+  };
+
+  const Todoitem = (props) => (
+    <TouchableOpacity
+      style={[styles.item, { backgroundColor: props.item.color }]}
+      onPress={() => markItemCompleted(props.item)}
+    >
+      <Text
+        style={
+          props.item.completed ? styles.itemTextCompleted : styles.itemText
+        }
+      >
+        {props.item.title}
+      </Text>
+    </TouchableOpacity>
+  );
+
+  const renderAddButton = () => {
+    return (
+      <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+        <View style={styles.icon}>
+          <Ionicons name="add" size={24} color="#652E00" />
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text>Sarah Goewey</Text>
+    <SafeAreaView style={styles.container}>
+      <Modal
+        visible={isModalVisible}
+        transparent={true}
+        onRequestClose={() => setIsModalVisible(!isModalVisible)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput
+              style={styles.input}
+              onChangeText={setText}
+              value={text}
+            />
+            <Button title="Add To-do" onPress={addNewToDo} />
+          </View>
+        </View>
+      </Modal>
       <StatusBar style="auto" />
-      <TextInput style={styles.input} />
-      <Button title="start" onPress={() => Alert.alert("Button pressed")} />
       <FlatList
-        data={DATA}
+        style={styles.list}
+        data={items}
         renderItem={({ item }) => <Todoitem item={item} />}
         keyExtractor={(item) => item.id}
+        ListFooterComponent={renderAddButton}
       />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    height: 40,
-    width: 200,
-    borderWidth: 1,
-    padding: 10,
-    borderColor: "gray",
-  },
-});
